1d765f774322fe75867dae49c40e513ace154306
tani3010 <tani3010@gmail.com>
2020-02-02 23:01:17 +0900

    Update stopwatch.hpp

--- cpp/stopwatch.hpp
+++ cpp/stopwatch.hpp
@@ -2,18 +2,22 @@
 #define __STOPWATCH_HPP__
 
 #include <chrono>
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
 
-template <typename RESOLUTION = std::chrono::duration<float> >
+template <typename RESOLUTION = std::chrono::seconds>
 class stopWatch {
 public:
-  stopWatch() : start(std::chrono::system_clock::now()), elapsed(0.0), digits(3) {}
+  stopWatch(const unsigned int digits_ = 3) : 
+      start(std::chrono::system_clock::now()),
+      elapsed(0.0),
+      digits(digits_),
+      unit(getResolutionString()) {}
   ~stopWatch() {}
-  inline void show(const std::string& unit = "s") {
+  inline void show() {
     end = std::chrono::system_clock::now();
     elapsed = std::chrono::duration_cast<RESOLUTION>(end - start).count();
-    std::cout << " elapsed: " << std::fixed << std::setprecision(digits)
+    std::cout << " elapsed: " << std::fixed << std::setprecision(digits_)
       << elapsed << "[" << unit << "]" << std::endl;
   }
   inline void reset() { start = std::chrono::system_clock::now(); }
@@ -21,6 +25,16 @@ private:
   const int digits;
   std::chrono::system_clock::time_point start, end;
   double elapsed;
+  std::string unit;
+  std::string getResolutionString() const {
+    if constexpr (std::is_same_v<RESOLUTION, std::chrono::nanoseconds>) return "ns";
+    if constexpr (std::is_same_v<RESOLUTION, std::chrono::microseconds>) return "us";
+    if constexpr (std::is_same_v<RESOLUTION, std::chrono::milliseconds>) return "ms";
+    if constexpr (std::is_same_v<RESOLUTION, std::chrono::seconds>) return "s";
+    if constexpr (std::is_same_v<RESOLUTION, std::chrono::minutes>) return "m";
+    if constexpr (std::is_same_v<RESOLUTION, std::chrono::hours>) return "h";
+    return "unknown";
+  }
 };
 
-#endif // __STOPWATCH_HPP__
+#endif  // __STOPWATCH_HPP__
